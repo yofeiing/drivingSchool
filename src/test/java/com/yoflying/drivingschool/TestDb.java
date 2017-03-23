d109b23d5c289bc3635d5c2b8ebe5fe7a6fd244e
arvin <arvin@turingcat.com>
2017-03-23 10:52:08 +0800

    Test

--- src/test/java/com/yoflying/drivingschool/TestDb.java
+++ src/test/java/com/yoflying/drivingschool/TestDb.java
@@ -1,7 +1,11 @@
 package com.yoflying.drivingschool;
 
+import com.yoflying.drivingschool.domain.dao.TokenMapper;
+import com.yoflying.drivingschool.domain.model.Token;
+import com.yoflying.drivingschool.domain.service.TokenService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,13 +13,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 /**
  * Created by liqiang on 16/12/18.
  */
-//@ContextConfiguration(locations = {"classpath:spring/spring**.xml"})
-//@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring/applicationContext.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
 //@ActiveProfiles("single")
 public class TestDb {
 
+    @Autowired
+    TokenService mapper;
     @Test
     public void test() {
-        System.out.println("111");
+        Token token = new Token();
+        token.setTokenCode("111");
+        token.setCategory(1);
+        token.setUserId(1L);
+        mapper.insertToken(token);
     }
 }
